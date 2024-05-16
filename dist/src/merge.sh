55edce6679df37ac36c22737a6ee7bc780bc3bf2
Younes <99495923+iamyounss@users.noreply.github.com>
2024-05-16 15:36:20 +0200

    fix: git restore json files

--- dist/src/merge.sh
+++ dist/src/merge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Usage: ./merge.sh <from_branch> <to_branch>
 
-FROM_BRANCH=$1
-TO_BRANCH=$2
+FROM_BRANCH=$1 #main
+TO_BRANCH=$2 #live-canada
 
 # Get the current commit hash
 commit_hash=$(git rev-parse --short HEAD)
@@ -11,13 +11,11 @@ commit_hash=$(git rev-parse --short HEAD)
 git checkout $TO_BRANCH && git pull origin $TO_BRANCH || git checkout -b $TO_BRANCH origin/$TO_BRANCH
 
 # Start the merge but don't commit automatically, favorise 'theirs' strategy option for conflicts
-if git merge --no-edit --no-commit --no-ff --strategy-option theirs --allow-unrelated-histories $FROM_BRANCH; then
+if git merge --no-commit --no-ff --strategy-option theirs --allow-unrelated-histories $FROM_BRANCH; then
     
-    # Checkout specific files from the current commit, ignoring errors
-    git checkout ${commit_hash} templates/\*.json 2>/dev/null || true
-    git checkout ${commit_hash} sections/\*.json 2>/dev/null || true
-    git checkout ${commit_hash} locales/\*.json 2>/dev/null || true
-    git checkout ${commit_hash} config/settings_data.json 2>/dev/null || true
+    # Remove modifications on JSON files from the current commit, ignoring errors
+    git restore --staged templates/*.json config/settings_data.json locales/*.json sections/*.json 2>/dev/null || true
+    git restore templates/*.json config/settings_data.json locales/*.json sections/*.json 2>/dev/null || true
 
     # If reset is successful and there are no conflicts
     if [ $? -eq 0 ]; then
@@ -25,6 +23,7 @@ if git merge --no-edit --no-commit --no-ff --strategy-option theirs --allow-unre
 
         # Push the changes to the 'to' branch on the origin remote
         git push --force origin $TO_BRANCH
+        git checkout $FROM_BRANCH
         echo "success" > merge-status.txt
     else
         echo "something goes wrong" > merge-status.txt
