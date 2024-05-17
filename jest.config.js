7d6898fde98f060c1c850b6baae61a8ad60d4218
Younes <99495923+iamyounss@users.noreply.github.com>
2024-05-17 16:44:17 +0200

    jest config

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    clearMocks: true,
+    fakeTimers: {},
+    moduleFileExtensions: ['js', 'ts'],
+    testEnvironment: 'node',
+    testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+    transform: {
+      '^.+\\.ts$': 'ts-jest'
+    },
+    verbose: true
+  }
