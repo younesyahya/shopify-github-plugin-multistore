9334c4a65cab7e029da27333cf22ed704ab76f71
Younes <99495923+iamyounss@users.noreply.github.com>
2024-05-21 21:35:49 +0200

    fix: ignore sections json files if doesnt eexists

--- src/merge.sh
+++ src/merge.sh
@@ -4,7 +4,6 @@
 FROM_BRANCH=$1 # main
 TO_BRANCH=$2 # live-canada
 
-
 # Configure Git user
 GIT_USER_NAME="GitHub Action: Multi Store Merge Bot"
 GIT_USER_EMAIL="bot@the-deployer.fr"
@@ -20,26 +19,33 @@ git fetch origin $FROM_BRANCH
 git fetch origin $TO_BRANCH
 
 # Checkout the source branch and pull the latest changes
-git checkout $FROM_BRANCH || git checkout -b $FROM_BRANCH origin/$FROM_BRANCH
+git checkout $FROM_BRANCH
 git pull origin $FROM_BRANCH
 
 # Checkout the target branch and pull the latest changes
-git checkout $TO_BRANCH || git checkout -b $TO_BRANCH origin/$TO_BRANCH
+git checkout $TO_BRANCH
 git pull origin $TO_BRANCH
 
 # Start the merge but don't commit automatically, favoring 'theirs' strategy option for conflicts
 if git merge --no-commit --no-ff --strategy-option theirs --allow-unrelated-histories $FROM_BRANCH; then
     
-    # Remove modifications on JSON files from the current commit, ignoring errors
-    git restore --staged templates/*.json config/settings_data.json sections/*.json 
-    git restore templates/*.json config/settings_data.json sections/*.json 
+    # Check if sections/*.json files exist
+    if find sections -name "*.json" | grep -q .; then
+        # Restore all JSON files if sections/*.json exist
+        git restore --staged templates/*.json config/settings_data.json sections/*.json 
+        git restore templates/*.json config/settings_data.json sections/*.json 
+    else
+        # Restore only templates and config JSON files if no sections/*.json exist
+        git restore --staged templates/*.json config/settings_data.json
+        git restore templates/*.json config/settings_data.json
+    fi
 
     # Commit the merge with a message
     git commit -m "GitHub Action: Merge $FROM_BRANCH into $TO_BRANCH"
     echo "Merge successful - pushing changes to $TO_BRANCH"
 
     # Push the changes to the 'to' branch on the origin remote
-    git push --force --set-upsteam origin $TO_BRANCH
+    git push --force --set-upstream origin $TO_BRANCH
 
     # Checkout the source branch
     git checkout $FROM_BRANCH
